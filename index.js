613c46a2858b558aa4387c4b536f2e6cbe10cdea
Akshit Awasthi <awasthiakshit11@gmail.com>
2021-01-27 20:27:39 +0530

    Fixed bugs and refactored the code

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
-const { exec } = require("child_process");
-const { stdout } = require("process");
+
+const {
+  exec
+} = require("child_process");
+const {
+  stdout
+} = require("process");
 const fs = require("fs");
 const chalk = require("chalk");
 const clear = require("clear");
@@ -10,104 +15,110 @@ const dataStore = require("./dataStore");
 
 clear();
 console.log(
-  chalk.green(figlet.textSync(`React Easy App`, { horizontalLayout: "full" }))
+  chalk.green(figlet.textSync(`React Easy App`, {
+    horizontalLayout: "full"
+  }))
 );
 console.log(
   chalk.green("Create a React app using parcel and minimum configuration.")
 );
 
 inquirer
-  .prompt([
-    {
-      name: "name",
-      type: "input",
-      message: "What do you want your project to be called?",
-    },
-    {
-      name: "version",
-      type: "input",
-      message: "What is the version of your project?",
-    },
-    {
-      name: "description",
-      type: "input",
-      message: "Describe your project.",
-    },
-    {
-      name: "author",
-      type: "input",
-      message: "Who is the author of project?",
-    },
-  ])
+  .prompt([{
+    name: "name",
+    type: "input",
+    message: "What do you want your project to be called?",
+  }, {
+    name: "version",
+    type: "input",
+    message: "What is the version of your project?",
+  }, {
+    name: "description",
+    type: "input",
+    message: "Describe your project.",
+  }, {
+    name: "author",
+    type: "input",
+    message: "Who is the author of project?",
+  }, ])
   .then((answer) => {
     answer.name === "" ? "" : (dataStore.projectName = answer.name);
     answer.version === "" ? "" : (dataStore.projectVer = answer.version);
-    answer.description === ""
-      ? ""
-      : (dataStore.projectDes = answer.description);
+    answer.description === "" ?
+      "" :
+      (dataStore.projectDes = answer.description);
     answer.author === "" ? "" : (dataStore.author = answer.author);
 
     console.log(dataStore);
-    exec(`mkdir ${dataStore.projectName}`, (err, stdout, stdin) => {
-      if (err) console.log(err);
-    });
-    exec(
-      `npm init -y`,
-      { cwd: dataStore.projectName },
-      (err, stdout, stdin) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
+  });
 
-        fs.writeFile(
-          `${dataStore.projectName}/package.json`,
-          `{
-    "name": "${dataStore.projectName}",
-    "version": "${dataStore.projectVer}",
-    "description": "${dataStore.projectDes}",
-    "main": "index.js",
-    "scripts": {
-      "test": "No test script was specified.",
-      "start": "${dataStore.startScript}"
-    },
-    "keywords": [],
-    "author": "${dataStore.author}",
-    "license": "ISC"
+exec(`mkdir ${dataStore.projectName}`, function(err, stdout, stdin) {
+
+  if (err) {
+    console.log(err);
+    return;
+  };
+});
+
+exec(
+  `cd ${dataStore.projectName} && npm init -y`,
+
+  function(err, stdout, stdin) {
+    if (err) {
+      console.log(err);
+      return;
+    };
+  });
+
+
+fs.writeFile(
+  `${dataStore.projectName}/package.json`,
+  `{
+          "name": "${dataStore.projectName}",
+          "version": "${dataStore.projectVer}",
+          "description": "${dataStore.projectDes}",
+          "main": "index.js",
+          "scripts": {
+            "test": "No test script was specified.",
+            "start": "${dataStore.startScript}"
+          },
+          "keywords": [],
+          "author": "${dataStore.author}",
+          "license": "ISC"
+          }`,
+  function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    };
+
   }
-  `,
-          (err) => {
-            if (err) {
-              console.log(err);
-              return;
-            }
-          }
-        );
+);
 
-        exec(
-          `cd ${dataStore.projectName} &&  npm i react react-dom parcel-bundler`
-        );
-        exec(
-          `cd ${dataStore.projectName} &&  npm i babel-preset-env babel-preset-react --save-dev`
-        );
-        fs.writeFile(
-          `${dataStore.projectName}/.babelrc`,
-          `
-    {
-    "presets": ["env", "react"]
-    }
-     `,
-          (err) => {
-            if (err) console.log(err);
-            return;
-          }
-        );
-        fs.mkdir(`${dataStore.projectName}/public`, (err) => {
-          if (err) console.log(err);
+exec(
+  `cd ${dataStore.projectName} &&  npm i react react-dom parcel-bundler`
+);
+exec(
+  `cd ${dataStore.projectName} &&  npm i babel-preset-env babel-preset-react --save-dev`
+);
+fs.writeFile(
+  `${dataStore.projectName}/.babelrc`,
+  `
+           {
+              "presets": ["env", "react"]
+            } 
+          `,
+  function(err) {
+    if (err) console.log(err);
+    return;
+  }
+);
+fs.mkdir(`${dataStore.projectName}/public`, function(err) {
+  if (err) console.log(err);
 
-          fs.writeFile(
-            `${dataStore.projectName}/public/index.html`,
-            `<!DOCTYPE html>
+  fs.writeFile(
+    `${dataStore.projectName}/public/index.html`,
+    `<!DOCTYPE html>
             <html>
               <head>
                 <title>React Easy App</title>
@@ -119,33 +130,39 @@ inquirer
               </body>
             </html>
             `,
-            (err) => {
-              if (err) console.log(err);
-            }
-          );
-        });
+    function(err) {
+      if (err) {
+        console.log(err);
+        return;
+      };
+    }
+  );
+});
 
-        fs.mkdir(`${dataStore.projectName}/src`, (err) => {
-          if (err) console.log(err);
+fs.mkdir(`${dataStore.projectName}/src`, function(err) {
+  if (err) console.log(err);
 
-          fs.writeFile(
-            `${dataStore.projectName}/src/style.css`,
-            `body {
+  fs.writeFile(
+    `${dataStore.projectName}/src/style.css`,
+    `body {
                 background-color: black;
                 color: white;
                 font-size: 28px;
                 text-align: center;
               }
               `,
-            (err) => {
-              if (err) console.log(err);
-            }
-          );
+    function(err) {
+      if (err) {
+        console.log(err);
+        return;
+      };
+    }
+  );
 
-          fs.writeFile(
-            `${dataStore.projectName}/src/index.js`,
+  fs.writeFile(
+    `${dataStore.projectName}/src/index.js`,
 
-            `import React from "react";
+    `import React from "react";
           import ReactDOM from "react-dom";
           
           class HelloMessage extends React.Component {
@@ -164,11 +181,11 @@ inquirer
           
           ReactDOM.render(<HelloMessage name="React Easy" />, App);
           `,
-            (err) => {
-              if (err) console.log(err);
-            }
-          );
-        });
-      }
-    );
-  });
+    function(err) {
+      if (err) {
+        console.log(err);
+        return;
+      };
+    }
+  );
+});
